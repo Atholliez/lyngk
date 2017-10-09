06facfaa12854be75046bd07d5a2155d07527ed5
Atholliez <alexandre.tholliez@gmail.com>
2017-10-09 14:11:16 +0200

    ajouts de plusieurs fonctions

--- src/Intersection.js
+++ src/Intersection.js
@@ -3,5 +3,33 @@
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c) {
-     this.State = 0;
+     var state = Lyngk.State.VACANT;
+     var color;
+     var pieces = [];
+
+     this.getState = function (){
+         return state;
+     }
+
+     this.color = function (){
+          return color;
+     }
+
+     this.piece = function(){
+          return piece;
+     }
+
+    this.pose = function(c)
+    {
+        if(pieces.length <=0)
+            state = Lyngk.State.ONE_PIECE;
+        else if(pieces.length > 0 && pieces.length < 4)
+            state = Lyngk.State.STACK;
+        else if(pieces.length >= 4)
+            state = Lyngk.State.FULL_STACK;
+        pieces.push(new Lyngk.Piece(c));
+        color = c;
+    }
+
+
 };
