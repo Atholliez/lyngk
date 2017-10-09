0086c863598a36a73f7a08f2bd7aa555484ea691
Atholliez <alexandre.tholliez@gmail.com>
2017-10-09 09:35:08 +0200

    deuxieme test

--- test/test.js
+++ test/test.js
@@ -5,4 +5,27 @@ var LyngkTestCase = TestCase("LyngkTestCase");
 LyngkTestCase.prototype.testA= function(){
     var c = new Lyngk.Coordinates("A",1);
     assertFalse(c.valid());
+
 };
+
+LyngkTestCase.prototype.testB= function(){
+
+    var cpt=0;
+    var lettre = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+
+    for(var c=0 ;c<lettre.length ;c++){
+        for (var i = 1; i < 10; i++) {
+            var coordonnee = new Lyngk.Coordinates(lettre[c], i);
+            if (coordonnee.valid()) {
+                cpt++;
+            }
+        }
+    }
+    assertEquals(cpt, 43);
+
+};
+
+
+
+
+
