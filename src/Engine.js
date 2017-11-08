cd7411cef251dc12a67764129b8c54c311ebc0b9
Atholliez <alexandre.tholliez@gmail.com>
2017-11-08 16:17:03 +0100

    ajouts des fonctions liées au déplacement (ne fonctionne pas)

--- src/Engine.js
+++ src/Engine.js
@@ -21,7 +21,6 @@ Lyngk.Engine = function () {
     };
 
 
-
     this.pleinPiece = function () {
         for (var coordonnee in coordoInters) {
             if (coordoInters.hasOwnProperty(coordonnee)) {
@@ -33,29 +32,28 @@ Lyngk.Engine = function () {
     };
 
 
-
     this.pleinPieceFull = function () {
-        var lettre = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
-        var tabValid = [ 'C1',
-            'E2','D2','C2','B2',
-            'G3','F3','E3','D3','C3','B3','A3',
-            'G4','F4','E4','D4','C4','B4',
-            'H5','G5','F5','E5','D5','C5','B5',
-            'H6','G6','F6','E6','D6','C6',
-            'I7','H7','G7','F7','E7','D7','C7',
-            'H8','G8','F8','E8',
+        var lettre = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+        var tabValid = ['C1',
+            'E2', 'D2', 'C2', 'B2',
+            'G3', 'F3', 'E3', 'D3', 'C3', 'B3', 'A3',
+            'G4', 'F4', 'E4', 'D4', 'C4', 'B4',
+            'H5', 'G5', 'F5', 'E5', 'D5', 'C5', 'B5',
+            'H6', 'G6', 'F6', 'E6', 'D6', 'C6',
+            'I7', 'H7', 'G7', 'F7', 'E7', 'D7', 'C7',
+            'H8', 'G8', 'F8', 'E8',
             'G9'];
 
-        var cptColor = [0,0,0,0,0,0];
-        for(var x=0; x<lettre.length;x++){
-            for(var y=0; y<10;y++){
-                if(tabValid.indexOf(lettre[x]+y) !== -1){
-                    var inter = new Lyngk.Intersection(x,y);
+        var cptColor = [0, 0, 0, 0, 0, 0];
+        for (var x = 0; x < lettre.length; x++) {
+            for (var y = 0; y < 10; y++) {
+                if (tabValid.indexOf(lettre[x] + y) !== -1) {
+                    var inter = new Lyngk.Intersection(x, y);
                     var rng;
-                    do{
-                        rng = Math.floor(Math.random()*6);
+                    do {
+                        rng = Math.floor(Math.random() * 6);
                     }
-                    while((cptColor[rng] === 8 && rng !== Lyngk.Color.WHITE) || (cptColor[rng] === 3 && rng=== Lyngk.Color.WHITE));
+                    while ((cptColor[rng] === 8 && rng !== Lyngk.Color.WHITE) || (cptColor[rng] === 3 && rng === Lyngk.Color.WHITE));
                     inter.poser(rng);
                     coordoInters.push(inter);
                     cptColor[rng]++;
@@ -66,7 +64,7 @@ Lyngk.Engine = function () {
 
     };
 
-    this.getColorFromPieces = function (i){
+    this.getColorFromPieces = function (i) {
         return coordoInters[i].getPieceColorFromPile();
     };
 
@@ -74,8 +72,55 @@ Lyngk.Engine = function () {
         return coordoInters.length;
     };
 
-    this.getTaillePileInter = function(i){
-       return coordoInters[i].getHeight();
+    this.getTaillePileInter = function (i) {
+        return coordoInters[i].getHeight();
+    };
+
+    this.posiCase = function (x) {
+        for (var y in coordoInters) {
+            if (coordoInters[y].getCoordo().toString() === x) {
+                return coordoInters[y];
+            }
+        }
+    };
+
+    this.deplacer = function (depuis, vers) {
+        var d = this.posiCase(depuis);
+        var v = this.posiCase(vers);
+        var vDep = this.valideDep(v,d);
+
+        return vDep;
+    };
+
+
+
+
+    this.ligneValide = function (p1,p2){
+        if (p1.getX() === p2.getX()) {
+            for (var x = p1.getY() - 9; x < 9; x++) {
+                if (p1.getHashedCoordo() === p2.getHashedCoordo() + x && x!==0) {
+                    return true;
+                }
+            }
+        }
+        if (p1.getY() === p2.getY()) {
+            for (var x = p1.getY() - 9; x < 9; x++) {
+                if (p1.getHashedCoordo() === p2.getHashedCoordo() + 10*x && x!==0) {
+                    return true;
+                }
+            }
+        }
+        for (var x = p1.getY() - 9; x < 9; x++) {
+            if (p1.getHashedCoordo() === p2.getHashedCoordo() + 11*x && x!==0) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    this.valideDep=function(p1,p2) {
+        var c = this.ligneValide(p1, p2);
+        return c;
     };
 
 };
